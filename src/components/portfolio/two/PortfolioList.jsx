dc04929cd74ce1c3d0586ba967166648e3ae33c8
rajeshofficial <rscdraj@gmail.com>
2024-10-30 19:14:44 +0000

    portfolio updated

--- src/components/portfolio/two/PortfolioList.jsx
+++ src/components/portfolio/two/PortfolioList.jsx
@@ -14,48 +14,56 @@ const portfolioListData = [
 		title: "Custom Web Development",
 		description: "Creating tailored websites that cater to specific business needs, ensuring unique functionality and design.",
 		img: Portfolio1Img,
+		path: "/React-website", // Unique path for each portfolio item
 	},
 	{
 		id: crypto.randomUUID(),
 		title: "UI/UX Design",
 		description: "Designing user-friendly and visually appealing interfaces for websites and mobile applications to enhance user engagement.",
 		img: Portfolio2Img,
+		path: "/ui-ux-design",
 	},
 	{
 		id: crypto.randomUUID(),
-		title: "Mobile App Development",
+		title: "Mobile Application Development",
 		description: "Building robust and responsive mobile applications that work seamlessly on Android and iOS platforms.",
 		img: Portfolio3Img,
+		path: "/mobile-app-development",
 	},
 	{
 		id: crypto.randomUUID(),
 		title: "WordPress Development",
 		description: "Creating versatile WordPress websites tailored for easy management and optimal performance.",
 		img: Portfolio4Img,
+		path: "/wordpress-portfolio",
 	},
 	{
 		id: crypto.randomUUID(),
 		title: "Wix Development",
 		description: "Designing and developing websites on Wix for quick, customizable, and user-friendly solutions.",
 		img: Portfolio5Img,
+		path: "/wix-web",
 	},
 	{
 		id: crypto.randomUUID(),
 		title: "Web Solutions",
 		description: "Providing all web-related solutions, from domain and hosting to website optimization and SEO services.",
 		img: Portfolio6Img,
+		path: "/",
 	},
 	{
 		id: crypto.randomUUID(),
-		title: "E-commerce Development",
+		title: "Shopify",
 		description: "Building responsive and feature-rich e-commerce sites with product management, payment gateways, and more.",
 		img: Portfolio7Img,
+		path: "/single-portfolio",
 	},
 	{
 		id: crypto.randomUUID(),
 		title: "Website Redesign",
 		description: "Revamping existing websites with updated design, functionality, and mobile responsiveness.",
 		img: Portfolio8Img,
+		path: "/React-website",
 	},
 ];
 
